5b9100c7937bd7ce1ce9e1c15385cc6a40c0da47
Harsh Mehta <deadspaceharsh@gmail.com>
2019-10-20 16:34:24 +0530

    Update game.py

--- game.py
+++ game.py
@@ -69,22 +69,22 @@ while True:
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     kernel = np.ones((3,3), np.uint)
     mask = cv2.inRange(hsv, Lower_green, Upper_green)
-    mask = cv2.erode(mask, kernel, iterations=2)
+    mask = cv2.erode(mask, kernel, iterations=1)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     mask = cv2.dilate(mask, kernel, iterations=5)
     #res = cv2.bitwise_and(img, img, mask = mask)
-    cnts, heir = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    main_image , heir = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
     center = None
 
-    if len(cnts) > 0:
-        c = max(cnts, key=cv2.contourArea)
+    if len(main_image) > 0:
+        c = max(main_image, key=cv2.contourArea)
         ((x,y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
         if radius > 0.5:
-            #cv2.circle(img, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-            cv2.circle(img, center, 5, (0, 0, 255), -1)
+            #cv2.circle(img, (int(x), int(y)), int(radius),(0, 0, 255), 2)
+            cv2.circle(img, center, 5, (0, 255, 255), -1)
 
     pts.appendleft(center)
     for i in range(1, len(pts)):
@@ -92,7 +92,7 @@ while True:
         if pts[i -1] is None or pts[i] is None:
             continue
         thick = int(np.sqrt(len(pts) / float(i + 1)) * 2.5)
-        cv2.line(img, pts[i - 1], pts[i], (0, 0, 255), thick)
+        cv2.line(img, pts[i - 1], pts[i], (255 , 255, 255), thick)
         #print(pts[i -1], pts[i])
         circleDraw(pts[i-1], pts[i])
 
@@ -103,8 +103,7 @@ while True:
 
 
     k = cv2.waitKey(30) % 0xFF
-    if k == 32:
-        break
+
 
 gameIntro()
 video_capture.release()
